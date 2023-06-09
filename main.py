8f7cc93718afc471313e005ae450f0263cae3b49
vitt0 <vitt0ne.jc@gmail.com>
2023-06-09 06:42:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from st_keyup import st_keyup
 from PIL import Image
 from decimal import Decimal
 import streamlit.components.v1 as components
-from streamlit.components.v1 import html
 import pandas as pd
 import urllib.request
 import asyncio
@@ -28,36 +27,6 @@ st.set_page_config(
         'About': None
     }
 )
-
-def copy_to_clipboard():
-    # Define the data to be copied
-    total_wood_amount = 100
-    num_wood = 5
-    total_stone_amount = 50
-    num_stones = 10
-
-    # Display the data
-    farm_info = st.empty()
-    farm_info.success(f"\n ⚒️ **Resources to be Gathered:**")
-    farm_info.write(f" - 🌲 **{total_wood_amount:.2f} Wood** — {num_wood}x Trees")
-    farm_info.write(f" - ⚪ **{total_stone_amount:.2f} Stone** — {num_stones}x Stones")
-
-    # Create a button to copy the data to clipboard
-    copy_button_code = """
-    function copyToClipboard() {{
-      const textToCopy = `{:.2f} Wood, ${}x Trees, {:.2f} Stone, ${}x Stones`;
-      navigator.clipboard.writeText(textToCopy)
-        .then(() => {{
-          console.log('Text copied to clipboard');
-        }})
-        .catch((error) => {{
-          console.error('Error copying text to clipboard:', error);
-        }});
-    }}
-    """.format(total_wood_amount, num_wood, total_stone_amount, num_stones)
-
-    copy_button = st.button("Copy to Clipboard", on_click="copyToClipboard")
-    st.components.v1.html("<script>{}</script>".format(copy_button_code))
 
 hide_menu_style = """
         <style>
@@ -1231,14 +1200,6 @@ with tab5:
                 if dawn_breaker_tickets_count > 0:
                     farm_info.write(f"- 🎟️ **{dawn_breaker_tickets_count} Dawn Breaker Ticket**")    
 
-                # Display the data
-                total_wood_amount = 100
-                num_wood = 5
-                total_stone_amount = 50
-                num_stones = 10
-
-                farm_info = st.empty()
-
                 farm_info.write("\n")   
                 farm_info.success(f"\n ⚒️ **Resources to be Gathered:**")
                 farm_info.write(f" - 🌲 **{total_wood_amount:.2f} Wood** — {num_wood}x Trees")
@@ -1247,8 +1208,6 @@ with tab5:
                 farm_info.write(f" - 🟡 **{total_gold_amount:.2f} Gold** — {num_gold}x Gold Rocks")
                 farm_info.write(f" - 🥚 **{total_chickens_amount:.2f} Eggs** — {num_chickens}x Chickens")
 
-                copy_to = copy_to_clipboard()
-                farm_info.write(f"{copy_to}")
 
                 #farm_info.write("\n")          
                 #farm_info.success(f"\n 📊 **Total Nodes:**")
