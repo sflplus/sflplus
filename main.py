761416f587f5c00321198f28a5a2b4df2994a717
vitt0 <vitt0ne.jc@gmail.com>
2023-06-09 05:38:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -574,10 +574,8 @@ fruits = ["Apple", "Orange", "Blueberry"]
 fruits_price = {"Apple": 0.3125, "Orange": 0.225, "Blueberry": 0.15}
 fruit_emojis = {"Apple": " \U0001F34E ", "Orange": " \U0001f34a ", "Blueberry": " \U0001f347 "}
 
-st.divider()
+
 tab5, tab6, tab7, tab8, tab9, tab10  = st.tabs(["💾HOME", "🏆RANKING", "👥BUMPKIN", "💸TRADER", "👨‍🔬CALCULATOR", "📜NFT LIST"])
-
-
 
 with tab5:
     # Define default farm ID
