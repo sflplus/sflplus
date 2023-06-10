8998236c70896ad4ba103f139162d16ff2b742b6
vitt0 <vitt0ne.jc@gmail.com>
2023-06-10 04:00:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2432,8 +2432,7 @@ with tab8:
     # Iterate over the crops list and display the crop cards
     for index, crop in enumerate(crops):
         markdown_content = """
-            <div class="card h-100">
-                <div class="card text-white bg-dark mb-3" style="max-width: 25rem;">
+                <div class="card text-white bg-dark mb-3 h-100" style="max-width: 25rem;">
                   <a href="{}{}" style="display: inline-block" target="_blank">
                     <img src="{}" alt="NFT Image" class="card-img-top" alt="..."></a>
                     <span class="w-100 p-2 bg-secondary position-relative bottom-0 text-center"><span class="badge text-center rounded-pill bg-success start-50" style="font-size:1rem;">Crop</span></span>                 
@@ -2445,7 +2444,6 @@ with tab8:
                     <span class="card-text"><b>Avg Price: {}</b></span>                    
                   </div>
                 </div>
-            </div>
         """.format(
             opensea_url_base,
             crop["url"],
