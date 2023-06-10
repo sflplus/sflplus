218009664a2e658a29f06b893381c910158628ef
vitt0 <vitt0ne.jc@gmail.com>
2023-06-10 00:51:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2304,11 +2304,11 @@ with tab8:
         },
     ]
     # Create the layout grid for the crop cards
-    col111, col112, col113, col114 = tab8.columns(4)
+    col111, col112, col113 = tab8.columns(3)
 
     # Iterate over the crops list and display the crop cards
     for index, crop in enumerate(crops):
-        with col111 if index % 4 == 0 else col112 if index % 4 == 1 else col113 if index % 4 == 2 else col114:
+        with col111 if index % 3 == 0 else col112 if index % 3 == 1 else col113:
             markdown_content = """
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
