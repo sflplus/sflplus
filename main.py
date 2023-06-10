eaa494cfa1ac1a7a6bcb354e430799d4a7f61630
vitt0 <vitt0ne.jc@gmail.com>
2023-06-10 17:59:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2443,14 +2443,8 @@ with tab8:
         
     # Define the tags dictionary
     tags_dict = {
-        "Crop": {
-            "color": "#28a745",
-            "emoji": "🌱",
-        },
-        "Potato": {
-            "color": "#c28606",
-            "emoji": "🥔",
-        },
+        "Crop": {"color": "#28a745", "emoji": "🌱"},
+        "Potato": {"color": "#c28606", "emoji": "🥔"},
         # Add more tag entries as needed
     }
 
@@ -2470,9 +2464,9 @@ with tab8:
     # Use the updated crop dictionary to display the crop cards
     for index, crop in enumerate(crops):
         type_badges = " ".join([
-            f'<span class="badge text-center rounded-pill start-50" style="font-size:1rem;background-color:{tags_info[tag.strip()]["color"]}">{tag}</span>'
+            f'<span class="badge text-center rounded-pill start-50" style="font-size:1rem;background-color:{tags_dict[tag.strip()]["color"]}">{tags_dict[tag.strip()]["emoji"]} {tag}</span>'
             for tag in crop["type"]
-            if tag.strip() in tags_info
+            if tag.strip() in tags_dict
         ])
 
         markdown_content = """
