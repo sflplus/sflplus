47d4e217330aec437afd71beb1443f0546e41dbd
vitt0 <vitt0ne.jc@gmail.com>
2023-06-11 03:12:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1988,11 +1988,12 @@ with tab6:
 
         
     live_minted2.info(f" 🐢 **Emerald Turtle [0/100]**")
-    live_minted2.write(f"- 🍾 **80** Old Bottles, 🌿 **50** Seaweed")
-    live_minted2.write(f"- 🧭 **30** Iron Compass and 💰 **100** SFL")
+    live_minted2.write(f"- 🍾 **80** Old Bottles - 🌿 **50** Seaweed")
+    live_minted2.write(f"- 🧭 **30** Iron Compass - 💰 **100** SFL")
+    live_minted2.write.write("\n")
     live_minted2.info(f" 🥫 **Tin Turtle [0/3000]**")
-    live_minted2.write(f"- 🍾 **50** Old Bottles, 🌿 **25** Seaweed")
-    live_minted2.write(f"- 🧭 **15** Iron Compass and 💰 **40** SFL")
+    live_minted2.write(f"- 🍾 **50** Old Bottles - 🌿 **25** Seaweed")
+    live_minted2.write(f"- 🧭 **15** Iron Compass - 💰 **40** SFL")
     
 url_rank1 = 'http://168.138.141.170:8080/api/v1/DawnBreakerTicket/ranking'
 #url_rank2 = 'http://168.138.141.170:8080/api/v1/DawnBreakerTicket/ranking' 
