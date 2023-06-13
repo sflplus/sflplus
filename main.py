e1522efc81f950bf910090ebfbe73c91f78dd258
vitt0 <vitt0ne.jc@gmail.com>
2023-06-13 20:51:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1458,16 +1458,17 @@ with tab5:
                                     total_harvests_left += harvests_left
                             #st.session_state[f"{name}_total_harvests_left"] = total_harvests_left
                             fruit_harvest_left_container = st.container()
-                            h_fruit.write(f"{fruit_harvest_left_container}")
-                            with h_fruit:
-                                with fruit_harvest_left_container:
-                                    h_fruit.write("\n")
-                                    h_fruit.info(f"\n{fruit_emojis.get(name)} **{name} Harvest Left: {total_harvests_left}**")
+                            with fruit_harvest_left_container:
+                                st.write("\n")
+                                st.info(f"\n{fruit_emojis.get(name)} **{name} Harvest Left:** {total_harvests_left}**")
 
-                                    for tree_id, tree_data in fruit_patches_dict.items():
-                                        if tree_data.get("fruit") and tree_data["fruit"].get("name") == name:
-                                            harvests_left = tree_data["fruit"].get("harvestsLeft")
-                                            h_fruit.write(f" - 🌳 **{harvests_left} Harvests Left**")
+                                for tree_id, tree_data in fruit_patches_dict.items():
+                                    if tree_data.get("fruit") and tree_data["fruit"].get("name") == name:
+                                        harvests_left = tree_data["fruit"].get("harvestsLeft")
+                                        st.write(f" - 🌳 **{harvests_left} Harvests Left**")
+
+                            #with h_fruit:
+                            h_fruit.write(fruit_harvest_left_container)
                 else:
                     h_fruit.error("\n **There aren't Fruit trees.**")
 
