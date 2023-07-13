21444a273b37ca6d36acddca6a97fc3453a92688
vitt0 <vitt0ne.jc@gmail.com>
2023-07-13 01:09:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -410,7 +410,7 @@ def wearable_price(item_name=None, return_type='result_df'):
     # clean the "Last Sale" values
     df['Last Sale'] = df['Last Sale'].str.replace('Last sale: ', '')
     for index, value in df['Last Sale'].items():
-        if '<' in value or '>' in value:
+        if isinstance(value, str) and ('<' in value or '>' in value):
             df.at[index, 'Last Sale'] = None
 
     # convert prices in Matic and ETH to USD
