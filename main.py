8c3580a9ad2f402afd33a952d1cfa3ed3337ef8b
vitt0 <vitt0ne.jc@gmail.com>
2023-07-25 01:14:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1398,7 +1398,8 @@ with tab5:
                             reward_sfl *= extra_boost
                             deliveryReward = f"{reward_sfl:.2f} SFL"
                         else:
-                            deliveryReward = f"{reward} 🎟️"
+                            reward_tickets = reward["tickets"]
+                            deliveryReward = f"🎟️ {reward_tickets} tickets"
                         if readytime and readytime > current_time:
                             remaining_time = readytime - current_time
                             hours_remaining = int(remaining_time / (1000 * 60 * 60))  # Convert milliseconds to hours
