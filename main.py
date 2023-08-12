93b431b8f731e7494a9e6813a7251c6da1370d9e
vitt0 <vitt0ne.jc@gmail.com>
2023-08-12 02:54:08 -0300

    New rankings

--- main.py
+++ main.py
@@ -6,21 +6,16 @@ from pandas import DataFrame
 import asyncio
 import aiohttp
 from datetime import datetime
-
 import streamlit as st
 from streamlit.delta_generator import DeltaGenerator
-
 from priceapi import PriceAPI
 from tabs.home import HomeTab
 from tabs.top import TopTab
 from tabs.bumpkin import BumpkinTab
 from tabs.ranking import RankingTab
-
 # if TYPE_CHECKING:
 #     from pandas import Series
-
 favicon: Any = Image.open("favicon.png")
-
 st.set_page_config(
     page_title="SFL Plus",
     page_icon=favicon,
@@ -28,7 +23,6 @@ st.set_page_config(
     initial_sidebar_state="expanded",
     menu_items={"Get Help": None, "Report a bug": None, "About": None},
 )
-
 # st.write(
 #     '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/
 # bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1a
@@ -36,27 +30,19 @@ st.set_page_config(
 # crossorigin="anonymous">',
 #     unsafe_allow_html=True,
 # )
-
-
 def local_css(file_name) -> None:
     with open(file_name, "r", encoding="utf-8") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-
 class Main:
     def __init__(self) -> None:
         self.version = "v3.2"
         self.priceAPI: PriceAPI = PriceAPI()
         local_css("style.css")
-
         self.eth_price: float = self.priceAPI.retrieve_eth_price()
         self.matic_price: float = self.priceAPI.retrieve_matic_price()
         self.sfl_price: float = self.priceAPI.retrieve_sfl_price()
-
         self.sfl_supply: int | None = self.priceAPI.get_token_supply()
-
         # self.API_KEY_DUNE = st.secrets["api_dune"]
-
         # self.queries_owners: list[Any] = [100, None, None]
         # self.queries: list[str] = ["2649121", "2649118", "2427499"]  #
         # self.queries_name: list[str] = [
@@ -71,7 +57,6 @@ class Main:
         # ]  #
         # self.queries_emoji: list[str] = ["🐢", "🥫", "🐌"]  #
         # queries_ticket = ["3200", "1200", "500"]
-
         app_state_temp: dict[
             str, list[str]
         ] = st.experimental_get_query_params()
@@ -81,7 +66,6 @@ class Main:
             k: v[0] if isinstance(v, list) else v
             for k, v in app_state_temp.items()
         }
-
         with open("data/skill_descriptions.json", "r", encoding="utf-8") as f:
             self.skills_description: dict[str, str] = json.load(f)
         with open("data/xp_data.json", "r", encoding="utf-8") as f:
@@ -96,7 +80,6 @@ class Main:
             self.emojis: dict[str, str] = json.load(f)
         with open("data/limits.json", "r", encoding="utf-8") as f:
             self.limits: dict[str, int] = json.load(f)
-
         self.fruits: list[str] = ["Apple", "Orange", "Blueberry"]
         self.fruits_price: dict[str, float] = {
             "Apple": 0.15625,
@@ -108,7 +91,6 @@ class Main:
             "Orange": " 🍊 ",
             "Blueberry": " 🍇 ",
         }
-
         TopTab(self)
         tab1: DeltaGenerator
         tab2: DeltaGenerator
@@ -116,26 +98,18 @@ class Main:
         tab1, tab2, tab3 = st.tabs(
             ["💾HOME", "🏆RANKING", "👥BUMPKIN"]
         )  # "📜NFT LIST", "👨‍🔬CALCULATOR", "💸TRADER"
-
         # Define default farm ID
-
         hometab = HomeTab(self, tab1)
         self.farm_id: str = hometab.get_farm_id()
         farm_tab_cons: dict[str, DeltaGenerator] = hometab.get_containers()
-
         ranktab = RankingTab(self, tab2)
         self.rank_tab_cons: dict[str, DeltaGenerator] = ranktab.get_containers()
         bumpkintab = BumpkinTab(self, tab3)
-
-
 url_rank1 = "http://168.138.141.170:8080/api/v1/DawnBreakerTicket/ranking"
-# url_rank2 = 'http://168.138.141.170:8080/api/v1/DawnBreakerTicket/ranking'
-
 
 async def fetch(url, session: aiohttp.ClientSession) -> dict:
     async with session.get(url, timeout=5) as response:
         return await response.json()
-
 
 async def main() -> None:
     main_app = Main()
@@ -154,90 +128,90 @@ async def main() -> None:
             {
                 "Farm": [farm["FarmID"] for farm in data1["farms"]],
                 "Bumpkin XP": [
-                    farm["BumpkinXP"]
-                    if "BumpkinXP" in farm
-                    and farm["BumpkinXP"] != ""
+                    farm["B_XP"]
+                    if "B_XP" in farm
+                    and farm["B_XP"] != ""
                     else None
                     for farm in data1["farms"]
                 ],
+                "Expansion": [
+                    farm["Land"]
+                    if "Land" in farm
+                    and farm["Land"] != ""
+                    else None
+                    for farm in data1["farms"]
+                ],
+                "SFL Earn": [
+                    farm["B_EA"]
+                    if "B_EA" in farm
+                    and farm["B_EA"] != ""
+                    else None
+                    for farm in data1["farms"]
+                ],
+                "SFL Spent": [
+                    farm["B_SP"]
+                    if "B_SP" in farm
+                    and farm["B_SP"] != ""
+                    else None
+                    for farm in data1["farms"]
+                ],                
             }
         )
 
-        # df2 = pd.DataFrame(
-        #     {
-        #         "Farm": [farm["FarmID"] for farm in data1["farms"]],
-        #         "Week 10": [
-        #             farm["LanternsCraftedByWeek"]["10"]
-        #             if "LanternsCraftedByWeek" in farm
-        #             and "10" in farm["LanternsCraftedByWeek"]
-        #             else None
-        #             for farm in data1["farms"]
-        #         ],
-        #         "Week 9": [
-        #             farm["LanternsCraftedByWeek"]["9"]
-        #             if "LanternsCraftedByWeek" in farm
-        #             and "9" in farm["LanternsCraftedByWeek"]
-        #             else None
-        #             for farm in data1["farms"]
-        #         ],
-        #         "Week 8": [
-        #             farm["LanternsCraftedByWeek"]["8"]
-        #             if "LanternsCraftedByWeek" in farm
-        #             and "8" in farm["LanternsCraftedByWeek"]
-        #             else 0
-        #             for farm in data1["farms"]
-        #         ],
-        #         "Week 7": [
-        #             farm["LanternsCraftedByWeek"]["7"]
-        #             if "LanternsCraftedByWeek" in farm
-        #             and "7" in farm["LanternsCraftedByWeek"]
-        #             else 0
-        #             for farm in data1["farms"]
-        #         ],
-        #         "Week 6": [
-        #             farm["LanternsCraftedByWeek"]["6"]
-        #             if "LanternsCraftedByWeek" in farm
-        #             and "6" in farm["LanternsCraftedByWeek"]
-        #             else 0
-        #             for farm in data1["farms"]
-        #         ],
-        #         "Week 5": [
-        #             farm["LanternsCraftedByWeek"]["5"]
-        #             if "LanternsCraftedByWeek" in farm
-        #             and "5" in farm["LanternsCraftedByWeek"]
-        #             else 0
-        #             for farm in data1["farms"]
-        #         ],
-        #         "Week 4": [
-        #             farm["LanternsCraftedByWeek"]["4"]
-        #             if "LanternsCraftedByWeek" in farm
-        #             and "4" in farm["LanternsCraftedByWeek"]
-        #             else 0
-        #             for farm in data1["farms"]
-        #         ],
-        #         "Week 3": [
-        #             farm["LanternsCraftedByWeek"]["3"]
-        #             if "LanternsCraftedByWeek" in farm
-        #             and "3" in farm["LanternsCraftedByWeek"]
-        #             else 0
-        #             for farm in data1["farms"]
-        #         ],
-        #         "Week 2": [
-        #             farm["LanternsCraftedByWeek"]["2"]
-        #             if "LanternsCraftedByWeek" in farm
-        #             and "2" in farm["LanternsCraftedByWeek"]
-        #             else 0
-        #             for farm in data1["farms"]
-        #         ],
-        #         "Week 1": [
-        #             farm["LanternsCraftedByWeek"]["1"]
-        #             if "LanternsCraftedByWeek" in farm
-        #             and "1" in farm["LanternsCraftedByWeek"]
-        #             else 0
-        #             for farm in data1["farms"]
-        #         ],
-        #     }
-        # )
+        df2 = pd.DataFrame(
+            {
+                "Farm": [farm["FarmID"] for farm in data1["farms"]],
+                "Sunflowers": [
+                    farm["B_SU"]
+                    if "B_SU" in farm
+                    and farm["B_SU"] != ""
+                    else None
+                    for farm in data1["farms"]
+                ],
+                "Trees": [
+                    farm["B_WO"]
+                    if "B_WO" in farm
+                    and farm["B_WO"] != ""
+                    else None
+                    for farm in data1["farms"]
+                ],
+                "Stone": [
+                    farm["B_ST"]
+                    if "B_ST" in farm
+                    and farm["B_ST"] != ""
+                    else None
+                    for farm in data1["farms"]
+                ],
+                "Iron": [
+                    farm["B_IR"]
+                    if "B_IR" in farm
+                    and farm["B_IR"] != ""
+                    else None
+                    for farm in data1["farms"]
+                ],
+                "Gold": [
+                    farm["B_GO"]
+                    if "B_GO" in farm
+                    and farm["B_GO"] != ""
+                    else None
+                    for farm in data1["farms"]
+                ],
+                "Shovels": [
+                    farm["B_SH"]
+                    if "B_SH" in farm
+                    and farm["B_SH"] != ""
+                    else None
+                    for farm in data1["farms"]
+                ],
+                "Drills": [
+                    farm["B_DR"]
+                    if "B_DR" in farm
+                    and farm["B_DR"] != ""
+                    else None
+                    for farm in data1["farms"]
+                ],                    
+            }
+        )
 
         # df3 = pd.DataFrame(
         #     {
@@ -269,95 +243,35 @@ async def main() -> None:
         df1: DataFrame = df1.dropna(subset=["Bumpkin XP"])
         # df3 = df3.dropna(subset=['Wild Mushroom'])
 
-        # # MAKE SURE TOP 10 SHOWS
-        # top_ten_ids = fetch_top_ten_ids()
-        # lanterns_data = retrieve_lanterns_data(top_ten_ids)
-        # #lanterns_data = {}
-        # existing_ids = df2['Farm'].tolist()
-        # new_ids = []
-
-        # for farm_id in lanterns_data.keys():
-        #     if farm_id not in existing_ids:
-        #         new_ids.append(farm_id)
-        #         lantern_data = lanterns_data[farm_id]
-        #         new_row = {
-        #             'Farm': farm_id,
-        #             'Week 8': lantern_data.get('8', 0),
-        #             'Week 7': lantern_data.get('7', 0),
-        #             'Week 6': lantern_data.get('6', 0),
-        #             'Week 5': lantern_data.get('5', 0),
-        #             'Week 4': lantern_data.get('4', 0),
-        #             'Week 3': lantern_data.get('3', 0),
-        #             'Week 2': lantern_data.get('2', 0),
-        #             'Week 1': lantern_data.get('1', 0)
-        #         }
-        #         df2 = pd.concat([df2, pd.DataFrame(new_row, index=[0])],
-        # ignore_index=True)
-        # Remove emphy columns
-        # df2: DataFrame = df2.dropna(axis=1, how="all")
-
-        # Count the number of farms that have crafted at least 5
-        # lanterns in all the weeks
-        # count_farms: int = (
-        #     (
-        #         df2[
-        #             [
-        #                 "Week 8",
-        #                 "Week 7",
-        #                 "Week 6",
-        #                 "Week 5",
-        #                 "Week 4",
-        #                 "Week 3",
-        #                 "Week 2",
-        #                 "Week 1",
-        #             ]
-        #         ]
-        #         >= 5
-        #     ).all(axis=1)
-        # ).sum()
-
-        # Count the number of farms that have crafted at least 5 lanterns
-        # in all the weeks
-        # count_farms2: int = (
-        #     (
-        #         df2[
-        #             [
-        #                 "Week 8",
-        #                 "Week 7",
-        #                 "Week 6",
-        #                 "Week 5",
-        #                 "Week 4",
-        #                 "Week 3",
-        #                 "Week 2",
-        #                 "Week 1",
-        #             ]
-        #         ]
-        #         >= 1
-        #     ).all(axis=1)
-        # ).sum()
-
         # Convert Total Ticket column to numeric values
         df1["Bumpkin XP"] = pd.to_numeric(df1["Bumpkin XP"])
         df1['Bumpkin XP'] = df1['Bumpkin XP'].round(2)
+        df1["Expansion"] = pd.to_numeric(df1["Expansion"])
+        df1["SFL Earn"] = pd.to_numeric(df1["SFL Earn"])
+        df1['SFL Earn'] = df1['SFL Earn'].round(2)
+        df1["SFL Spent"] = pd.to_numeric(df1["SFL Spent"])
+        df1['SFL Spent'] = df1['SFL Spent'].round(2)
 
         
         # Create a new column "Level" in the DataFrame
-        # df1["Level"] = None  # Initialize the "Level" column with None
+        df1["Level"] = None  # Initialize the "Level" column with None
         
         # Iterate through the DataFrame rows and determine the level based on "Bumpkin XP"
-        # for index, row in df1.iterrows():
-        #     bump_xp = row["Bumpkin XP"]
-        #     current_lvl = None
+        for index, row in df1.iterrows():
+            bump_xp = row["Bumpkin XP"]
+            current_lvl = None
             
-        #     for level, info in self.xp_dict.items():
-        #         if bump_xp >= info["Total XP"]:
-        #             current_lvl = level
+            for level, info in main_app.xp_dict.items():
+                if bump_xp >= info["Total XP"]:
+                    current_lvl = level
         
-        #     if current_lvl is None:
-        #         current_lvl = max(self.xp_dict.keys())
+            if current_lvl is None:
+                current_lvl = max(main_app.xp_dict.keys())
             
-        #     df1.at[index, "Level"] = current_lvl
-
+            df1.at[index, "Level"] = current_lvl
+        
+        #reorder columns
+        df1 = df1[["Farm", "Level", "Bumpkin XP", "Expansion", "SFL Earn", "SFL Spent"]]
 
         
         # df2["Week 8"] = pd.to_numeric(df2["Week 8"])
@@ -380,36 +294,36 @@ async def main() -> None:
 
         # Sort by Total Ticket in descending order
         df1 = df1.sort_values(by="Bumpkin XP", ascending=False)
-        # df2 = df2.sort_values(by="Week 8", ascending=False)
+        df2 = df2.sort_values(by="Sunflowers", ascending=False)
         # df3: DataFrame = df3.sort_values(by="Points", ascending=False)
         # ['Bottles', 'Iron C.', 'Seaweed'],
         # ascending=[False, False, False], kind='mergesort')
 
-        # df2 = df2.rename(columns={"Week 8": "Week 8 🔻"})
-        # df3= df3.rename(columns={"Points": "Points 🔻"})
+        df1 = df1.rename(columns={"Bumpkin XP": "Bumpkin XP 🔻"})
+        df2 = df2.rename(columns={"Sunflowers": "Sunflowers 🔻"})
 
         # Reset index and set the "Ranking" column as the new index
         df1 = df1.reset_index(drop=True)
-        # df2 = df2.reset_index(drop=True)
+        df2 = df2.reset_index(drop=True)
         # df3 = df3.reset_index(drop=True)
 
         df1.index = df1.index + 1
-        # df2.index = df2.index + 1
+        df2.index = df2.index + 1
         # df3.index = df3.index + 1
 
         # Rename the index to "Ranking"
         df1.index.name = "Rank"
-        # df2.index.name = "Rank"
+        df2.index.name = "Rank"
         # df3.index.name = "Rank"
 
         # Convert index to integer values
         df1.index = df1.index.astype(int)
-        # df2.index = df2.index.astype(int)
+        df2.index = df2.index.astype(int)
         # df3.index = df3.index.astype(int)
 
         if df1.empty:
             main_app.rank_tab_cons["live_update"].error(
-                "The ranking is currently not working, it will be fixed soon™ "
+                " The ranking is currently not working, it will be fixed soon™ "
             )
         else:
             in_fmt = "%Y-%m-%dT%H:%M:%S.%fZ"
@@ -420,56 +334,18 @@ async def main() -> None:
             main_app.rank_tab_cons["live_update"].success(
                 f"🕘Updated at: **{update} UTC**"
             )
-
-            # if buttonok2:
-            #     df1 = df1.loc[df1["Farm"].str.contains(text_search)]
-            #     main_app.rank_tab_cons["live_ranking"].write(df1)
-            #     df2 = df2.loc[df2["Farm"].str.contains(text_search)]
-            #     main_app.rank_tab_cons["live_lantern"].write(df2)
-            #     main_app.rank_tab_cons["live_minted"].info(
-            #         f"🕯️ **Farms with 1 Lantern each week: {count_farms2}**"
-            #     )
-            #     main_app.rank_tab_cons["live_minted"].success(
-            #         f"🏮 **Farms with 5 Lanterns each week: {count_farms}**"
-            #     )
-            #     df3 = df3.loc[df3["Farm"].str.contains(text_search)]
-            #     main_app.rank_tab_cons["live_treasure"].write(df3)
-            # else:
+          
             main_app.rank_tab_cons["live_xp"].write(df1)
-            # main_app.rank_tab_cons["live_lantern"].write(df2)
-            # main_app.rank_tab_cons["live_minted"].info(
-            #     f"🕯️ **Farms with 1 Lantern each week: {count_farms2}**"
-            # )
-            # main_app.rank_tab_cons["live_minted"].success(
-            #     f"🏮 **Farms with 5 Lanterns each week: {count_farms}**"
-            # )
-            # main_app.rank_tab_cons["live_treasure"].data_editor(
-            #     df3,
-            #     column_config={
-            #         "Points": st.column_config.ProgressColumn(
-            #             "Points 🔻",
-            #             help="Click here to change the ranking based in "
-            #             + "Points",
-            #             # width="medium",
-            #             format="%.2f",
-            #             min_value=0,
-            #             max_value=100,
-            #         ),
-            #     },
-            #     hide_index=False,
-            #     disabled=True,
-            # )
+            main_app.rank_tab_cons["live_resources"].write(df2)
         pass
     except Exception as e:
         main_app.rank_tab_cons["live_update"].error(
-            f"The ranking is currently not working, it will be fixed soon™, "
+            f" 3 The ranking is currently not working, it will be fixed soon™, "
             + f"Error: {str(e)}"
         )
-
-
 if __name__ == "__main__":
     asyncio.run(main())
-
+    
 # with tab8:
 #     col_nft, buff11 = st.columns([2,2])
 #     with col_nft:
