d1c105bfa3e619a3948d706ed91d9da149018cf6
vitt0 <vitt0ne.jc@gmail.com>
2023-10-16 18:56:53 -0300

    Replace Top tab info

--- tabs/top.py
+++ tabs/top.py
@@ -18,116 +18,25 @@ class TopTab:
     def __init__(self, main) -> None:
         self.main: Main = main
         col00: DeltaGenerator
-        col11x: DeltaGenerator
         col0x: DeltaGenerator
-        col10x: DeltaGenerator
-        col00, col11x, col0x, col10x = st.columns([1.85, 1.75, 1.75, 3.5])
+        col00, col0x, buff3 = st.columns([1.85, 4, 3])
         col00.markdown(
             "[![Foo](https://sflplus.info/sflplus.png)](https://sflplus.info)"
             + '<span style="vertical-align:bottom;color:rgb(0, 221, 66);'
             + f'font-weight:bold;">{main.version}</span>',
             unsafe_allow_html=True,
         )
-        with col11x:
-            general_info: DeltaGenerator = st.container()
-            if main.sfl_supply is not None:
-                format_supply: str = "{:,.0f}".format(main.sfl_supply / 1e18)
-                supply_progress: float = 10000000 - (
-                    float(format_supply.replace(",", "")) - 30000000
-                )
-            else:
-                general_info.error(
-                    "Failed to get SFL Supply. Please try again later."
-                )
-                format_supply = "N/A"
-                supply_progress = 0
         with col0x:
             general_info2: DeltaGenerator = st.container()
 
-        with col10x:
-            general_info3: DeltaGenerator = st.container()
-
-            supply_progress_per: float = (supply_progress / 10000000) * 100
-            supply_percentage_float = float(supply_progress_per)
-            supply_percentage_number: str = "{:.2f}".format(
-                supply_percentage_float
+            general_info2.markdown(
+                f"🔒 We are sorry to let you know that the developent of "
+                + f"[SFL+]({self.DC_GITHUB_URL}) has come to a end and is "
+                + f"not longer going to be updated, read more about "
+                + f"[here]({self.DC_THREAD_URL}). We recomend you to use "
+                + f"[sfl.world](https://sfl.world) instead as web alternative "
+                + f"or [Nancy]({self.DC_NANCY_URL}) for automatic "
+                + f"notifications on Discord/Telegram."
             )
-            supply_progress_percentage: float = 100 - float(
-                supply_percentage_number
-            )
-            # supply_percentage_final: float = (
-            #     float(supply_percentage_number) / 100
-            # )
-            # supply_percentage_inv: float = 1 - supply_percentage_final
-
-            # current_time = datetime.now().timestamp()
-            # timestamp3 = 1688947200
-            # dt3 = datetime.fromtimestamp(timestamp3)
-
-            # time_remaining3 = dt3 - datetime.fromtimestamp(current_time)
-            # days_remaining3 = time_remaining3.days
-            # hours_remaining3 = time_remaining3.seconds // 3600
-            # minutes_remaining3 = (time_remaining3.seconds % 3600) // 60
-
-            # formatted_time_remaining2 = "{} Days and {:02d}:{:02d} hours".
-            # format(days_remaining3, hours_remaining3, minutes_remaining3)
-
-            # Mushrooms time
-            first_respawn = 1682899200
-            respawn_interval: timedelta = timedelta(hours=16)
-            current_time: float = datetime.now().timestamp()
-            respawns: float = (
-                current_time - first_respawn
-            ) // respawn_interval.total_seconds()
-            next_respawn: datetime = (
-                datetime.fromtimestamp(first_respawn)
-                + (respawns + 1) * respawn_interval
-            )
-            time_remaining: timedelta = next_respawn - datetime.fromtimestamp(
-                current_time
-            )
-            hours_remaining = int(time_remaining.total_seconds() // 3600)
-            minutes_remaining = int(
-                (time_remaining.total_seconds() % 3600) // 60
-            )
-
-            general_info.write(
-                f" 🟣 Matic: **{main.matic_price:.2f}** - "
-                + f"🌻 SFL: **{main.sfl_price:.4f}**"
-            )
-            general_info.write(
-                "🍄 Next Respawn in: **{:02d}:{:02d} hrs**".format(
-                    hours_remaining, minutes_remaining
-                )
-            )
-            general_info.markdown(
-                f"⚙️ Contribute to SFL+ on [Github]({self.DC_GITHUB_URL})"
-            )
-
-            general_info2.write(f" 📈 Current Supply: **{format_supply}**")
-            general_info2.write(" ⏳ Next Halvening: **40,000,000**")
-            general_info2.write(
-                f" 📊 In Percentage: **{supply_progress_percentage:.2f}%**"
-            )
-
-            # general_info2.write("⏳ **{}**".format(formatted_time_remaining2))
-            # general_info2.write(f" ⏳ {formatted_time_remaining2}")
-            # general_info.progress(supply_percentage_inv, text=None)
-            # features_info = st.expander("📗 **FEATURES**", expanded=False)
 
         st.divider()
-        general_info3.markdown(
-            "💬 Feedback? Tag me `@Vitt0c` or use this "
-            + f"[Discord thread]({self.DC_THREAD_URL})"
-        )
-        general_info3.markdown(
-            "☕ Donations Wallet: `0x24C262a7c49F8BBc889688A0cb0Fea97d04839c5`"
-        )
-        # features_info.write(f" - 💎 Farm Worth  - 🏝️ Farm Resources \n -
-        #  🚜 Hoarder Limit - 🤑 Spent Checker \n - 👜 Basket Checker
-        # - 💰 SFL Balance \n - 🍒 Fruits Harvest Left  - 👨‍🌾 Bumpkins Stats \n
-        #  - 🐔 Mutant Chickens ")
-        general_info3.markdown(
-            "🤖 Looking for automatic notifications on Discord or Telegram?"
-            + f" Try out [Nancy]({self.DC_NANCY_URL}) by `@Maschs`"
-        )
